c3f9a2d534af8bd2db27aeb0c5941399d4cf7519
moogle <vr@riseup.net>
2015-11-10 10:26:03 +0800

    Display a  simple window

---
+++ Demo_4.java
@@ -0,0 +1,14 @@
+package liang08;
+
+import javax.swing.JFrame;
+
+public class Demo_4 {
+  public static void main(String[] args) {
+    JFrame fame1 = new JFrame();
+    fame1.setTitle("Window 1");
+    fame1.setSize(200, 150);
+    fame1.setLocation(200, 300);
+    fame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    fame1.setVisible(true);
+  }
+}
